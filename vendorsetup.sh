19a4825246b79f7c387db6f6eb122ab1c9538d34
MusoZorin <muso20132013@gmail.com>
2026-04-22 11:59:25 +0500

    Clean up vendorsetup.sh by removing c
    orsetup.sh.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,1 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-
 add_lunch_combo twrp_a13-eng
