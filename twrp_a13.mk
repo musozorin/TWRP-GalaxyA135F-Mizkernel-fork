236a764fe80991c3702a7c4429b2967a824b7749
MusoZorin <muso20132013@gmail.com>
2026-04-22 11:59:11 +0500

    Update twrp_a13.mk

--- twrp_a13.mk
+++ twrp_a13.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Kernel is ARM64, core should then be 64 Bit
