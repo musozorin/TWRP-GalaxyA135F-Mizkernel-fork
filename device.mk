447853d18266bb2b0ba0b690a7d30f870d683787
MusoZorin <muso20132013@gmail.com>
2026-04-22 12:06:26 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,1 +1,21 @@
 LOCAL_PATH := device/samsung/a13
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+
+PRODUCT_PROPERTY_OVERRIDES +=\
+	ro.fastbootd.available=true
+	ro.boot.dynamic_partitions=true 
+	
+
+TW_INCLUDE_FASTBOOTD := true
+
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
+
+
+TW_INCLUDE_PYTHON := true
+TW_EXCLUDE_APEX := trueLOCAL_PATH := device/samsung/a13
